62141f8db0c79115cc05af92bd305a87158c7d29
FreezePeace1 <dromann2017@yandex.ru>
2024-03-07 17:26:13 +0300

    Partials for Index.cshtml

---
+++ WebStoreMVC/Components/UserViewComponent.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStoreMVC.Domain.Entities;
+
+namespace WebStoreMVC.Components;
+
+public class UserViewComponent : ViewComponent
+{
+   /*public IViewComponentResult Invoke()
+   {
+      return User.Identity.IsAuthenticated == true ? View() : View();
+   }*/
+
+}
